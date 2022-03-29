c8886c38fd1c7593e90d74ca723b3729a25a74e1
羽卒 <5575298+scx69@user.noreply.gitee.com>
2022-03-29 10:28:55 +0800

    更改打包配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
+const isProduction = process.env.NODE_ENV === 'production'
+
 module.exports = {
+  publicPath: isProduction ? `./` : '/',
+  assetsDir: './static',
   lintOnSave: false, // 关闭eslint检查
   productionSourceMap: false, //生产环境是否生成 sourceMap 文件
   devServer: {
