a3d61fbfeb4966894e8a19ab5c14f93ffe2b426a
羽卒 <5575298+scx69@user.noreply.gitee.com>
2022-03-29 09:31:32 +0800

    更改基础配置

--- src/api/index.js
+++ src/api/index.js
@@ -1,43 +1,48 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = 'http://127.0.0.1:7777';
-axios.defaults.withCredentials = true;
-// axios.defaults.headers["Content-Type"] = "application/x-www-form-urlencoded";
-axios.defaults.headers["Content-Type"] = "application/json";
-axios.defaults.timeout = 6000;
+const service = axios.create({
+  baseURL:
+    process.env.NODE_ENV === "production" ? "https://m.xiaomiyoupin.com" : "",
+  withCredentials: true,
+  timeout: 8 * 1000,
+});
 
-/*
 // 在向服务器发送前，修改请求数据
-axios.defaults.transformRequest = data => {
+/*axios.defaults.transformRequest = data => {
   if (!data) return data;
   let result = ``;
   for (let attr in data) {
     result += `&${attr}=${data[attr]}`;
   }
   return result.substring(1);
-};
+};*/
 
 // 请求拦截器
 axios.interceptors.request.use(
-  config => {
+  (config) => {
+    if (!config.headers["Content-Type"]) {
+      // application/x-www-form-urlencoded;charset=UTF-8
+      config.headers["Content-Type"] = "application/json";
+    }
     return config;
   },
-  error => {
+  (error) => {
     return Promise.reject(error);
   }
 );
-*/
+
 // 响应拦截器
-axios.interceptors.response.use(
-  response => {
+service.interceptors.response.use(
+  (response) => {
     return response.data;
   },
-  reason => {
+  (reason) => {
     return Promise.reject(reason);
   }
 );
-axios.defaults.validateStatus = status => {
+
+service.defaults.validateStatus = (status) => {
   return /^(2|3)\d{2}$/.test(status);
 };
 
-export default axios;
+export default service;
--- vue.config.js
+++ vue.config.js
@@ -5,22 +5,24 @@ module.exports = {
     port: 9999,
     open: true, // 浏览器自动打开
     hotOnly: false, // 热更新
+    compress: true, // 启动gzip压缩
+    hot: true, // 开启HMR
     proxy: {
       // 拦截以 /api 开头的请求并替换为 target
       "/home": {
         target: "https://m.xiaomiyoupin.com", //代理地址
         changeOrigin: true, // 接口跨域
         pathRewrite: {
-          "^/home": ""
-        }
+          "^/home": "",
+        },
       },
       "/server": {
         target: "http://127.0.0.1:7777",
         changeOrigin: true,
         pathRewrite: {
-          "^/server": ""
-        }
-      }
-    }
-  }
+          "^/server": "",
+        },
+      },
+    },
+  },
 };
